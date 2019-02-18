fe24fb4045869e497b16f8efcbdeffe402a4ef13
Flicker <libmi@foxmail.com>
2019-02-18 14:07:23 +0800

    create eureka server

---
+++ eureka-server/src/main/java/cn/notemi/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package cn.notemi.eurekaserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	static Logger logger = LoggerFactory.getLogger(EurekaServerApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+		logger.info("【Spring Cloud Eureka Server 启动完成】");
+	}
+
+}
+
